1a9b2f82d54730d5c5d31ba7d83ca5088a964ed3
Nano0nFire <nakahata.ryunosuke@gmail.com>
2025-02-15 00:10:43 +0900

    [Fix] General : Transport
    ver 0.1.065

--- Assets/_Scripts/UI/UI_PlayerDataManager.cs
+++ Assets/_Scripts/UI/UI_PlayerDataManager.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using Unity.Netcode.Transports.UTP;
 using Cysharp.Threading.Tasks;
+using Unity.Networking.Transport.Relay;
 
 public class UI_PlayerDataManager : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class UI_PlayerDataManager : MonoBehaviour
     [SerializeField] TMP_Dropdown WorldDropdown;
     [SerializeField] TMP_InputField AddressField;
     [SerializeField] TMP_InputField PortField;
+    [SerializeField] bool AllowRemote, UseRelay;
     [SerializeField] List<string> WorldNames;
     NetworkManager nwManager;
 
@@ -69,21 +71,34 @@ public class UI_PlayerDataManager : MonoBehaviour
         StartGamePanel.SetActive(true);
     }
 
+    public void AllowRemoteConnections(bool value) => AllowRemote = value;
+    public void ToggleUseRelay(bool value) => UseRelay = value;
+
     public async void StartHost()
     {
         var transport = nwManager.GetComponent<UnityTransport>();
+
         transport.ConnectionData.Port = PortField.text == string.Empty ? (ushort)7777 : ushort.Parse(PortField.text);
+        transport.ConnectionData.ServerListenAddress = AllowRemote ? "0.0.0.0" : "127.0.0.1";
         transport.ConnectionData.Address = AddressField.text == string.Empty ? "127.0.0.1" : AddressField.text;
 
         await UniTask.Delay(1000);
 
-        Debug.Log($"Start Host\nAddress : {transport.ConnectionData.Address}\nPort : {transport.ConnectionData.Port}");
+        Debug.Log($"Start Host\nAddress : {transport.ConnectionData.Address}\nPort : {transport.ConnectionData.Port}\nPort : {transport.ConnectionData.ServerListenAddress}");
         nwManager.StartHost();
         nwManager.SceneManager.LoadScene(WorldNames[WorldDropdown.value], LoadSceneMode.Single);
     }
 
-    public void StartClient()
+    public async void StartClient()
     {
+        var transport = nwManager.GetComponent<UnityTransport>();
+        transport.ConnectionData.Port = PortField.text == string.Empty ? (ushort)7777 : ushort.Parse(PortField.text);
+        transport.ConnectionData.ServerListenAddress = AllowRemote ? "0.0.0.0" : "127.0.0.1";
+        transport.ConnectionData.Address = AddressField.text == string.Empty ? "127.0.0.1" : AddressField.text;
+
+        await UniTask.Delay(1000);
+
+        Debug.Log($"Start Client\nAddress : {transport.ConnectionData.Address}\nPort : {transport.ConnectionData.Port}\nPort : {transport.ConnectionData.ServerListenAddress}");
         nwManager.StartClient();
     }
 }
